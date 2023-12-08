8193d54176f1fd8d0f84c5b33a9b00665b011329
prsj99-wiley <psanjeewa@wiley.com>
2023-12-09 01:35:47 +0530

    2nd commit

---
+++ terraform/resource_creation.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "terraform-state-bucket2-prsj"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::terraform-state-bucket2-prsj/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
+}
